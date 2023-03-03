a8d8b977f82f4ee0f094442ea70d04c762f3e9a0
José Luis Bautista Martín <joseluisbautista@gmail.com>
2023-03-03 16:47:10 -0600

    Commit inicial

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+
+//Establecer un certificado que no es de confianza
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback =  HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+
+
+// The port number must match the port of the gRPC server.
+var channel = GrpcChannel.ForAddress("https://localhost:5001",  new GrpcChannelOptions { HttpHandler = httpHandler });
+var client = new Greeter.GreeterClient(channel);
+
+// LLamo al servicio
+var reply = await client.SayHelloAsync( new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
